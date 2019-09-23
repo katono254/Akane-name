d12158869128dc873cbc4db304885c7be4083335
jakesIII <jayruma@yahoo.com>
2019-09-23 10:17:12 +0300

    Add java script styles to the page

--- java/script.js
+++ java/script.js
@@ -1,1 +1,17 @@
-var 
+var male=["Kwasi","Kwadwo", "Kwabena", "Kwaku", "Kwaku", "Yaw", "Kofi", "Kwame",];
+var female=["Akosua", "Adwoa", "Abenna", "Akua", "Yaa", "Afua", "Ama"];
+function flopper() {
+    var greg=""
+    var gender=document.forms["myForm"]["gender"].value;
+    var date=document.forms["myForm"]["bday"].value;
+    var charger=new Date(date);
+    var katono=charger.getDay();
+    if (gender==="male"){
+        greg=male[katono];
+    }else if(gender===female){
+        greg=female[katono];
+    }else{
+        alert("invalid option")
+    }
+    alert(greg)
+}
